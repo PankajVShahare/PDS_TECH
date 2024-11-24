041597390c930aa03c8b25f0a3dbd3c7343c3a8a
PankajVShahare <pankajshahare12@gmail.com>
2024-11-24 18:03:26 +0530

    release v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'PDS_TECH';
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      let outerContainer = document.querySelector('.outerContainer') as HTMLElement;
+      if(outerContainer) {
+        outerContainer.scrollIntoView({behavior: "smooth", block: "start" });
+      }
+    }, 1000);
+
+  }
 }
